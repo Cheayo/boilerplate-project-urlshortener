7ba8180e32a39d961ab9f7dc6a55c3dfe45c84ad
Lessa Winston <lessa.winston@protonmail.com>
2021-06-30 23:08:06 +0000

    added get and post

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const dns = require('dns');
+const bodyParser = require('body-parser');
+
+var urllist = [];
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -9,6 +13,8 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.use('/api/shorturl', bodyParser.urlencoded({extended: false}));
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -18,6 +24,29 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.get('/api/shorturl/:urlkey', function(req, res) {
+  if (isNaN(req.params.urlkey) || parseInt(req.params.urlkey) >= urllist.length) {
+    res.json({error: "Shortened URL does not exist."});
+  } else {
+    res.redirect(urllist[req.params.urlkey]);
+  }
+});
+
+app.post('/api/shorturl', function(req, res) {
+  let hostname = req.body.url.split('/')[2]
+  
+  dns.lookup(hostname, function(err, address, family) {
+    if(err) {
+      res.json({error: 'invalid url'});
+    } else {
+      let shorturl = urllist.push(req.body.url)
+
+      res.json({original_url: req.body.url, short_url: shorturl});
+    }
+  })
+});
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
